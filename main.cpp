f15ea45abe62fb83384066d9990b1c4a99edec62
jmercklen <mercklenjeremy@gmail.com>
2022-12-16 11:10:38 +0100

    MP23T1-9 fonction principale main()

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*////////////////////////////////////////////////////////////////////////////*/
+/*                                                                            */
+/*        main.cpp                                                            */
+/*                                                                            */
+/*   By: Jérémy Mercklen                                                      */
+/*                                                                            */
+/*   Created: 2022/12/16 by J. Mercklen                                       */
+/*   Updated: 2022/12/16 by J. Mercklen                                       */
+/*                                                                            */
+/*////////////////////////////////////////////////////////////////////////////*/
+
+#include "JeuNewton.h"
+
+int main(int argc, char** argv)
+{
+    void(argc);
+    void(argv);
+    JeuNewton partie;
+    return 0;
+}
